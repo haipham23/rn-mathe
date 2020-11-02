b2b6eb647050e37585eefb96b11e8d0e185afcbc
Hai Pham <haipham@Hais-MBP.fritz.box>
2020-11-02 13:25:11 +0000

    add eslint

---
+++ screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+import { SIZES } from "../constants";
+
+const Home = () => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ paddingVertical: SIZES.padding, alignItems: "center" }}>
+      <Text>Home</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default Home;
---
+++ screens/Settings.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+import { SIZES } from "../constants";
+
+const Settings = () => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ paddingVertical: SIZES.padding, alignItems: "center" }}>
+      <Text>Settings</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default Settings;
